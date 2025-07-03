ac40c0bea5085231b05aeb8da8fb9fb0605952ee
Closertech001 <danielajayi842@gmail.com>
2025-07-03 16:14:28 +0100

    Create symspell_setup.py

---
+++ symspell_setup.py
@@ -0,0 +1,9 @@
+# symspell_setup.py
+from symspellpy import SymSpell, Verbosity
+
+sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
+sym_spell.load_dictionary("frequency_dictionary_en_82_765.txt", term_index=0, count_index=1)
+
+def correct_query(text):
+    suggestions = sym_spell.lookup_compound(text, max_edit_distance=2)
+    return suggestions[0].term if suggestions else text
