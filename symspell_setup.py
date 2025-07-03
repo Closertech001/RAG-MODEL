5a946be5cc776865fe69ebe994378a71fae2e3b9
Closertech001 <danielajayi842@gmail.com>
2025-07-04 00:15:08 +0100

    Update symspell_setup.py

--- symspell_setup.py
+++ symspell_setup.py
@@ -1,6 +1,8 @@
 # symspell_setup.py
+
 from symspellpy import SymSpell, Verbosity
 
+# Load SymSpell dictionary
 sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
 sym_spell.load_dictionary("frequency_dictionary_en_82_765.txt", term_index=0, count_index=1)
 
